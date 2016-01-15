1936b29a70f70dffbb48aba6d9207a132edcf8b6
Allison <allison.knierim@gmail.com>
2016-01-15 08:34:37 -0700

    Variable and Functions

---
+++ DuckHunt/Assets/Scripts/VarandFunc.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class VarandFunc : MonoBehaviour {
+
+	public int myInt = 20000;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+		myInt = DivideByTwo (myInt);
+		Debug.Log (myInt);
+	}
+
+	int DivideByTwo (int number)
+	{
+		int ret;
+		ret = number * 2;
+		return ret;
+	}
+}
